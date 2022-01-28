63cc491ca9079a9c51a84dc52ba41ce01996188f
AndreySpakhi <andreyspakhi@ukr.net>
2022-01-28 22:16:29 +0200

    fix ALLOWED_HOSTS

--- greetingservice/settings.py
+++ greetingservice/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-_rb@_#p(h1o41w96h78^=w!ai)y&+wqjio)!)*w5@(%)v6^yh5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
